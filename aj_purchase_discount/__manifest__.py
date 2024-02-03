ce9d312e45b3f9a853c1334fe290b3601229f0fe
Arunja96 <arunj9095@gmail.com>
2024-02-03 19:24:26 +0530

    [update] manifest banner and description

--- aj_purchase_discount/__manifest__.py
+++ aj_purchase_discount/__manifest__.py
@@ -2,13 +2,15 @@
     'name': 'Purchase Discount',
     'version': '16.0.1.0.0',
     'summary': 'Discount fields for purchase',
+    'description': """
+    Add a discount for purchase line item """,
     'category': 'Inventory/Purchase',
     'author': 'Arun',
     'depends': ['purchase', 'account'],
     'data': [
         'views/purchase_order_view.xml',
     ],
-    'images': ['static/description/icon.png'],
+    'images': ['static/description/banner.png'],
     'assets': {
 
         'web.assets_backend': [
