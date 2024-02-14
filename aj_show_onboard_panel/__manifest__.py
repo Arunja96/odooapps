48c30db4cc8ca52d4052a4df8214773048f63ab5
Arunja96 <arunj9095@gmail.com>
2024-02-14 09:15:17 +0530

    [add] new module

---
+++ aj_show_onboard_panel/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Show Onboarding Panel',
+    'version': '15.0.1.0.0',
+    'summary': 'Easily close or open onboarding in sales, accounting etc.',
+    'description': """
+    Show the closed onboarding panel in sales, account. go to companies in setting select not done on onboarding
+     panel Tab.""",
+    'depends': ['sale', 'account',],
+    'data': [
+        'views/res_company.xml',
+    ],
+    'category': 'Uncategorized',
+    'author': 'Arun',
+    'images': ['static/description/banner.gif'],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
